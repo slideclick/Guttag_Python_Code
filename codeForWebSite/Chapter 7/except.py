d49a780c85ba1db07ff9d0634c584ac85b31724d
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-08-19 12:18:52 +0800

    adding first version

---
+++ codeForWebSite/Chapter 7/except.py
@@ -0,0 +1,21 @@
+def getRatios(vect1, vect2):
+    """Assumes: vect1 and vect2 are lists of equal length of numbers
+       Returns: a list containing the meaningful values of 
+             vect1[i]/vect2[i]"""
+    ratios = []
+    for index in range(len(vect1)):
+        try:
+            ratios.append(vect1[index]/float(vect2[index]))
+        except ZeroDivisionError:
+            ratios.append(float('nan')) #nan = Not a Number
+        except:
+            raise ValueError('getRatios called with bad arguments')
+    return ratios
+
+#Page 88
+try:
+    print getRatios([1.0,2.0,7.0,6.0], [1.0,2.0,0.0,3.0])
+    print getRatios([], [])
+    print getRatios([1.0, 2.0], [3.0])
+except ValueError, msg:
+    print msg
