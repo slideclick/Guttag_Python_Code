07ffc5872dddcdf8c64e2fa02df71612caef657a
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-08-19 12:24:35 +0800

    raise ValueError('getRatios called with bad arguments') from e

--- codeForWebSite/Chapter 7/except.py
+++ codeForWebSite/Chapter 7/except.py
@@ -9,13 +9,13 @@ def getRatios(vect1, vect2):
         except ZeroDivisionError:
             ratios.append(float('nan')) #nan = Not a Number
         except Exception as e:
-            raise ValueError('getRatios called with bad arguments')
+            raise ValueError('getRatios called with bad arguments') from e
     return ratios
 
 #Page 88
 try:
-    print getRatios([1.0,2.0,7.0,6.0], [1.0,2.0,0.0,3.0])
-    print getRatios([], [])
-    print getRatios([1.0, 2.0], [3.0])
-except ValueError, msg:
-    print msg
+    print (getRatios([1.0,2.0,7.0,6.0], [1.0,2.0,0.0,3.0]))
+    print (getRatios([], []))
+    print (getRatios([1.0, 2.0], [3.0]))
+except ZeroDivisionError as msg:
+    print (msg)
